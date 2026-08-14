084064f0f3c57f7003934465b3dda898da354cf7
sabarivel-ganesan_avtr <sabarivel.ganesan@avantorsciences.com>
2026-08-14 14:14:20 +0530

    new changes

--- src/main/java/code/CI_CD/service.java
+++ src/main/java/code/CI_CD/service.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class service {
     public String get_1() { return "Vanakam"; }
-    public String get_2() { return "Hello Vanakam"; }
+    public String get_2() { return "Hello"; }
     public String get_3() { return "Hi"; }
 }
