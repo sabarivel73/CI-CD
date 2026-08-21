560b28e5c39a6e4572946760043b55c967a209b2
sabarivel-ganesan_avtr <sabarivel.ganesan@avantorsciences.com>
2026-08-21 18:32:47 +0530

    new changes

--- src/test/java/code/CI_CD/CiCdApplicationTests.java
+++ src/test/java/code/CI_CD/CiCdApplicationTests.java
@@ -35,7 +35,7 @@ class CiCdApplicationTests {
 	public void test2() throws Exception {
 		mockMvc.perform(get("/get_2"))
 				.andExpect(status().isOk())
-				.andExpect((ResultMatcher) content().string("Hello"));
+				.andExpect((ResultMatcher) content().string("Vanakam Hello"));
 	}
 
 	@Test
@@ -43,7 +43,7 @@ class CiCdApplicationTests {
 	public void test3() throws Exception {
 		mockMvc.perform(get("/get_3"))
 				.andExpect(status().isOk())
-				.andExpect((ResultMatcher) content().string("Hi"));
+				.andExpect((ResultMatcher) content().string("Vanakam Hi"));
 	}
 
 }
